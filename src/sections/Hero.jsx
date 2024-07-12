a2481b6c6f6a1a2c5df9f4bc220b8a9bc94a9e7f
IT21356704 <it21356704@my.sliit.lk>
2024-07-12 23:04:23 +0530

    Added some Tailwind cutomized classes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Button from "../components/Button";
 import { arrowRight } from "../assets/icons";
-import { statistics } from "../constants";
+import { shoes, statistics } from "../constants";
+import { bigShoe1 } from "../assets/images";
+import ShoeCard from "../components/ShoeCard";
 
 const Hero = () => {
   return (
@@ -19,7 +21,7 @@ const Hero = () => {
             The New Arrival
           </span>
           <br />
-          <span className='text-red-500 inline-block mt-3'>Nike</span> Shoes
+          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
         </h1>
         <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
           Discover stylish Nike arrivals, quality comfort, and innovation for
@@ -35,6 +37,30 @@ const Hero = () => {
               <p className='leading-7 font-montserrat text-slate-gray'>
                 {stat.label}
               </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="relative flex-1 flex justify-center item-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img 
+        src={bigShoe1}
+        alt="shoe collection"
+        width={610}
+        height={500}
+        className="object-contain
+        relative z-10"
+        />
+
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%]">
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard 
+              imgURL={shoe}
+              changeBigShoeImage=
+              {() => {}}
+              bigShoeImg=""
+              />
             </div>
           ))}
         </div>
