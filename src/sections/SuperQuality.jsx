6aeda595392e3897cc353ac70d10285dd6d0ff65
IT21356704 <it21356704@my.sliit.lk>
2024-07-13 12:25:17 +0530

    Leftside of super Quality products completed

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Button from '../components/Button'
 
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section 
+    id='about-us'
+    className='flex justify-between items-center max-lg:flex-col gap-10 w-full max-container'>
+      <div className='flex flex-1 flex-col'>
+
+        <h2 className='font-palanquin text-4xl capitalize font-bold lg:max-w-lg'>
+          We Provide You
+          <span className='text-coral-red'> Super</span>
+          <span className='text-coral-red'> Quality</span> shoes
+        </h2>
+        <p className='mt-4 lg:max-w-lg info-text'>
+          Ensuring premium comfort and style, our meticulously crafted footware is designed to elevate yor experiences, providing you with the unmatched quality, innovation, and a touch of elegance.
+        </p>
+        <p className='mt-6 lg:max-w-lg info-text'>
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className='mt-11'>
+        <Button label='View details'  />
+        </div>
+
+      </div>
+    </section>
   )
 }
 
